486f8773bd4283d7fdc6e3262de74441418782dd
Ty Clements <tclements14@huskers.unl.edu>
2025-09-02 09:13:13 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 /**
- * Author: Terry Joseph Clements
+ * Author: Terry Joseph Clements! Abracadabra!
+ *
  * Date: 26 August 2025
  *
  * A simple hello world program in C
@@ -8,6 +9,8 @@
  #include <stdlib.h>
  #include <stdio.h>
 int main (int argc, char **argv) {
-    printf("Hello World/n");
+    printf("Hello World\n");
+    printf("Abracadabra\n");
+    prin
     return 0;
 }
