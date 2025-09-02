c1b9c7a01ffaa22f63c07f96ecbad86bcc01a088
Ty Clements <tclements14@huskers.unl.edu>
2025-09-02 08:21:16 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Terry Joseph Clements
+ * Date: 26 August 2025
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+int main (int argc, char **argv) {
+    printf("Hello World/n");
+    return 0;
+}
