3b761dd40a3e3e59d6acb575b495bb22fed9683d
lenacarnley <lcarnley2@huskers.unl.edu>
2025-09-02 22:36:21 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,16 +1,21 @@
 /**
  * Author: Terry Joseph Clements! Abracadabra!
+ * Author: Lena Carnley
  *
  * Date: 26 August 2025
+ * Date: 09/02/2026
  *
  * A simple hello world program in C
  *
  */
  #include <stdlib.h>
  #include <stdio.h>
+ 
 int main (int argc, char **argv) {
     printf("Hello World\n");
+    printf("Lena Carnley\n");
+    printf("undecided major\n");
     printf("Abracadabra\n");
-    prin
+
     return 0;
 }
